950884d2380b0e25855642322608a91289a94328
wavilikhin <44665164+wavilikhin@users.noreply.github.com>
2021-07-06 17:14:08 +0300

    Adds ci variant of db setup

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -11,30 +11,31 @@ export const getMongoConfig = async (
 };
 
 const getMongoString = (configService: ConfigService) => {
-    console.log(
-        `========\n\n\nTesting env ${configService.get('CI')}========\n\n\n`,
-    );
-    return (
-        'mongodb://' +
-        configService.get('MONGO_HOST') +
-        ':' +
-        configService.get('MONGO_PORT') +
-        '/' +
-        configService.get('MONGO_AUTHDATABASE')
-    );
+    switch (configService.get('CI')) {
+        case true:
+            return (
+                'mongodb://' +
+                configService.get('MONGO_HOST') +
+                ':' +
+                configService.get('MONGO_PORT') +
+                '/' +
+                configService.get('MONGO_AUTHDATABASE')
+            );
+        default:
+            return (
+                'mongodb://' +
+                configService.get('MONGO_LOGIN') +
+                ':' +
+                configService.get('MONGO_PASSWORD') +
+                '@' +
+                configService.get('MONGO_HOST') +
+                ':' +
+                configService.get('MONGO_PORT') +
+                '/' +
+                configService.get('MONGO_AUTHDATABASE')
+            );
+    }
 };
-
-// const getMongoString = (configService: ConfigService) =>
-// 'mongodb://' +
-// configService.get('MONGO_LOGIN') +
-// ':' +
-// configService.get('MONGO_PASSWORD') +
-// '@' +
-// configService.get('MONGO_HOST') +
-// ':' +
-// configService.get('MONGO_PORT') +
-// '/' +
-// configService.get('MONGO_AUTHDATABASE');
 
 const getMongoOptions = () => ({
     useNewUrlParser: true,
