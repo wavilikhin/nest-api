861f25a13a35e949f9db0994e82e856e64097f64
wavilikhin <44665164+wavilikhin@users.noreply.github.com>
2021-07-06 17:19:31 +0300

    Testing

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -10,42 +10,24 @@ export const getMongoConfig = async (
     };
 };
 
-const getMongoString = (configService: ConfigService) => {
-    switch (configService.get('CI')) {
-        case true:
-            console.log(
-                `\n\n######################\nUSING MONGO FOR CI, CI=${configService.get(
-                    'CI',
-                )}`,
-            );
-            return (
-                'mongodb://' +
-                configService.get('MONGO_HOST') +
-                ':' +
-                configService.get('MONGO_PORT') +
-                '/' +
-                configService.get('MONGO_AUTHDATABASE')
-            );
-        default:
-            console.log(
-                `\n\n######################\nUSING MONGO FOR PROD, CI=${configService.get(
-                    'CI',
-                )}`,
-            );
-            return (
-                'mongodb://' +
-                configService.get('MONGO_LOGIN') +
-                ':' +
-                configService.get('MONGO_PASSWORD') +
-                '@' +
-                configService.get('MONGO_HOST') +
-                ':' +
-                configService.get('MONGO_PORT') +
-                '/' +
-                configService.get('MONGO_AUTHDATABASE')
-            );
-    }
-};
+const getMongoString = (configService: ConfigService) =>
+    configService.get('CI')
+        ? 'mongodb://' +
+          configService.get('MONGO_HOST') +
+          ':' +
+          configService.get('MONGO_PORT') +
+          '/' +
+          configService.get('MONGO_AUTHDATABASE')
+        : 'mongodb://' +
+          configService.get('MONGO_LOGIN') +
+          ':' +
+          configService.get('MONGO_PASSWORD') +
+          '@' +
+          configService.get('MONGO_HOST') +
+          ':' +
+          configService.get('MONGO_PORT') +
+          '/' +
+          configService.get('MONGO_AUTHDATABASE');
 
 const getMongoOptions = () => ({
     useNewUrlParser: true,
