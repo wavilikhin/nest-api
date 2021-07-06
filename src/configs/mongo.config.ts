f339aaa356f6cdd330da065c4274547540b06845
wavilikhin <44665164+wavilikhin@users.noreply.github.com>
2021-07-06 17:04:47 +0300

    testing

--- src/configs/mongo.config.ts
+++ src/configs/mongo.config.ts
@@ -12,15 +12,21 @@ export const getMongoConfig = async (
 
 const getMongoString = (configService: ConfigService) =>
     'mongodb://' +
-    configService.get('MONGO_LOGIN') +
-    ':' +
-    configService.get('MONGO_PASSWORD') +
-    '@' +
     configService.get('MONGO_HOST') +
     ':' +
     configService.get('MONGO_PORT') +
     '/' +
     configService.get('MONGO_AUTHDATABASE');
+// 'mongodb://' +
+// configService.get('MONGO_LOGIN') +
+// ':' +
+// configService.get('MONGO_PASSWORD') +
+// '@' +
+// configService.get('MONGO_HOST') +
+// ':' +
+// configService.get('MONGO_PORT') +
+// '/' +
+// configService.get('MONGO_AUTHDATABASE');
 
 const getMongoOptions = () => ({
     useNewUrlParser: true,
